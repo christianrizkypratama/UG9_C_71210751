fb30acf70f70d4ad59eaa08625294ae2c5661f70
christianrp <93330021+christianrizkypratama@users.noreply.github.com>
2021-11-12 08:31:46 +0700

    Add files via upload

---
+++ 4_C_71210751.py
@@ -0,0 +1,9 @@
+print("Menghitung jumlah suku ke -99")
+
+a1 = int(input("Masukkan deret awal: "))
+a2= int(input("Masukkan deret kedua: "))
+a3= int(input("Masukkan deret ketiga: "))
+
+Sn = a3/2 * (2 * a1+(a3-1) * a2)
+
+print("Jumlah suku ke-99: ", Sn)
